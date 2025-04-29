798e3fbec28a670759492017bb36704e1d5fe4e2
AlexeyFef <alexfef72@gmail.com>
2025-04-29 22:25:59 +0200

    fix fixtures

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -2,11 +2,11 @@ from playwright.sync_api import Page, expect
 
 def test_main_actions(page: Page):
     page.get_by_test_id("search__input").fill(("python"))   # вводим в поле поиска "Искать на Литрес" значение "python"
-    page.keyboard.press("Enter")                            # нажимаем ввод
+    page.keyboard.press("Enter")                            # нажимаем  ввод
 
     expect(page).to_have_url("https://www.litres.ru/search/?q=python")  # проверяем URL
     page.locator("xpath=//*[@aria-description='Книги, которые можно читать без ограничений с активной Литрес: "
-                 "Подпиской']").click()               # по xpath находим и переключаем switcher "Доступно по подписке"
+                 "Подпиской']").click()               # по xpath  находим и переключаем switcher "Доступно по подписке"
     page.locator("xpath=//*[@aria-description='Книги, которые можно взять по Литрес: Абонементу']").click()
             #  по xpath находим и переключаем switcher "Доступно в абонементе"
 
